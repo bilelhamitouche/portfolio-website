8a6237cc2e28de48dc78f0495cf1f4b2e53b043d
Bilel Hamitouche <bilelhamitouche>
2025-01-06 17:32:37 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+function App({}: Props) {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
