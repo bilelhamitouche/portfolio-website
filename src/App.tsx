6cf7440525a29f501bc175bcb7133aa2f1cb3c2e
Bilel Hamitouche <bilelhamitouche>
2025-01-08 16:31:01 +0100

    added skills component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Skills from "./components/Skills";
 
 type Props = {};
 
@@ -10,6 +11,7 @@ function App({}: Props) {
       <Navbar />
       <main className="py-28">
         <Hero />
+        <Skills />
       </main>
       <Footer />
     </div>
