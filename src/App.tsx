8fca3a96d47c0ce0cd671cdf5a6ed02efd10a7ff
Bilel Hamitouche <bilelhamitouche>
2025-01-08 14:56:05 +0100

    added the footer and hero into the main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 type Props = {};
 
@@ -6,6 +8,10 @@ function App({}: Props) {
   return (
     <div className="container mx-auto max-w-screen-xl">
       <Navbar />
+      <main className="py-28">
+        <Hero />
+      </main>
+      <Footer />
     </div>
   );
 }
