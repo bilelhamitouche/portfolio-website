1ecd1d5ee186cce40ee87f61bb3882eccec639f2
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-02-10 06:06:32 +0100

    made the default theme to light

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en" className="scroll-smooth" data-theme="light">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
