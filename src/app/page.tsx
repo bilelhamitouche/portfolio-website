be86b81f20bfb35cf96ed5955d5f57430575282d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-02-11 00:59:14 +0100

    added the new components to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Footer from "@/components/Footer";
 import Navbar from "../components/Navbar";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <main className="container mx-auto max-w-6xl">
         <Hero />
         <Projects />
+        <Skills />
+        <Contact />
       </main>
       <Footer />
     </div>
