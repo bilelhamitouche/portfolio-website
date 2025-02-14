864cfa7e0e9579f2b2ebee54f819dfaa7d24f9d8
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-02-14 06:04:06 +0100

    refactoring types and adding skills variable instead of hardcoding

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,35 +1,41 @@
+import Skill from "@/types/skill";
 import SkillsCard from "./SkillsCard";
 
 function Skills() {
+  const skills: Skill[] = [
+    {
+      title: "Frontend",
+      stack: [
+        "Typescript",
+        "React",
+        "NextJS",
+        "TailwindCSS",
+        "DaisyUI",
+        "SCSS",
+      ],
+    },
+    {
+      title: "Backend",
+      stack: ["NodeJS", "Express", "PostgreSQL", "Prisma", "REST API"],
+    },
+    {
+      title: "Tools",
+      stack: ["Linux", "Jest", "Insomnia", "Neovim", "Markdown"],
+    },
+    {
+      title: "DevOps",
+      stack: ["Vercel", "Git", "Docker", "GitHub", "Bash"],
+    },
+  ];
   return (
     <section id="skills" className="py-12 pr-4 pl-8 md:py-24 lg:py-32">
       <h2 className="mb-12 text-3xl font-bold text-center sm:text-4xl md:text-5xl lg:text-6xl">
         Tech Stack
       </h2>
       <div className="grid grid-cols-1 gap-8 place-content-center md:grid-cols-2">
-        <SkillsCard
-          title="Frontend"
-          stack={[
-            "Typescript",
-            "React",
-            "NextJS",
-            "TailwindCSS",
-            "DaisyUI",
-            "SCSS",
-          ]}
-        />
-        <SkillsCard
-          title="Backend"
-          stack={["NodeJS", "Express", "PostgreSQL", "Prisma", "REST API"]}
-        />
-        <SkillsCard
-          title="Tools"
-          stack={["Linux", "Jest", "Insomnia", "Neovim", "Markdown"]}
-        />
-        <SkillsCard
-          title="DevOps"
-          stack={["Vercel", "Git", "Docker", "GitHub", "Bash"]}
-        />
+        {skills.map((skill, index) => (
+          <SkillsCard key={index} title={skill.title} stack={skill.stack} />
+        ))}
       </div>
     </section>
   );
