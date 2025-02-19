0eb5123d2795d5f4ff8984762abeffe879b5d9b6
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-02-19 02:55:19 +0100

    fixed the fonts (removed the css variable)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -25,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth" data-theme="light">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body className={`${geistSans.className}`}>{children}</body>
     </html>
   );
 }
