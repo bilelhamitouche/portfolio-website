993b3230046cd4eeb9ff0fe8ca11adc4d785a5d0
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-02-26 01:39:00 +0100

    replace about link with blog link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 function Navbar() {
   const navLinks: NavLink[] = [
     {
-      text: "About",
-      link: "#about",
+      text: "Blog",
+      link: "/blog",
     },
     {
       text: "Projects",
