a1d0c5dd8c404102afe93d6f8907ca626f6513a5
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-02-27 20:11:56 +0100

    put the blog section in the main app component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
+import Blogs from "@/components/Blogs";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <Hero />
         <Projects />
         <Skills />
+        <Blogs />
         <Contact />
       </main>
       <Footer />
