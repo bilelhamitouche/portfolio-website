6b8ce0ea23a0b5f410d47274a962019ac38ad9e1
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-03-02 17:38:22 +0100

    remove unused imports for navbar and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import Footer from "@/components/Footer";
-import Navbar from "../components/Navbar";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
