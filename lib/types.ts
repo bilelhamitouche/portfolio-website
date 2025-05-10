6c5e81201f66c7e5634d930b443b40388a9c3598
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-10 20:11:19 +0100

    added types for projects and tech stack data

---
+++ lib/types.ts
@@ -0,0 +1,13 @@
+export interface TechStackCardProps {
+  title: string;
+  stack: string[];
+}
+
+export interface ProjectCardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+  demoLink: string;
+  githubLink: string;
+  stack: string[];
+}
