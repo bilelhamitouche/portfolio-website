b2c1e86297eb776c2a6ff06ffb23825730107f05
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-10 21:01:32 +0100

    add descriptions to the sections

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,12 @@ export default function Projects() {
       id="projects"
       className="container py-12 px-4 mx-auto space-y-12 sm:py-16 md:py-20"
     >
-      <h2 className="text-3xl font-bold text-center">Work</h2>
+      <div className="space-y-4 text-center">
+        <h2 className="text-3xl font-bold">Work</h2>
+        <p className="text-lg text-gray-500">
+          Check out the list of projects I&apos;ve worked on
+        </p>
+      </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         <ProjectCard
           title="ChatSync"
--- components/TechStack.tsx
+++ components/TechStack.tsx
@@ -6,7 +6,12 @@ export default function TechStack() {
       id="tech-stack"
       className="container py-12 px-4 mx-auto space-y-8 sm:py-16 lg:py-20"
     >
-      <h2 className="text-3xl font-bold text-center">Tech Stack</h2>
+      <div className="space-y-4 text-center">
+        <h2 className="text-3xl font-bold">Tech Stack</h2>
+        <p className="text-lg text-gray-500">
+          These are the technologies I use every day
+        </p>
+      </div>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
         <TechStackCard
           title="Frontend"
