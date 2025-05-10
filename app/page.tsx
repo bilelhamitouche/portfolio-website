512f476054b2c91a5a7f5909752e9ae391160bc0
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-10 20:13:24 +0100

    add projects and tech stack section into page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Projects from "@/components/Projects";
+import TechStack from "@/components/TechStack";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Projects />
+      <TechStack />
       <Footer />
     </>
   );
