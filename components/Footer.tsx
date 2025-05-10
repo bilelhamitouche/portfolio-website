b03edb6e2797295205ac655e9dfb4917f78d1c56
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-10 16:39:01 +0100

    create a footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="p-4 text-center">
+      <p className="text-gray-500">
+        Copyright &copy; {new Date().getFullYear()} - All Rights Reserved By
+        Bilel Hamitouche
+      </p>
+    </footer>
+  );
+}
