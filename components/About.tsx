ccdca0eeca5b950fc132bdbef297293950f1e005
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-10 21:01:36 +0100

    add about section text

---
+++ components/About.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <section className="container py-12 px-4 mx-auto space-y-12 max-w-2xl text-center sm:py-16 lg:py-20">
+      <h2 className="text-3xl font-bold">About Me</h2>
+      <p className="text-lg text-gray-500">
+        T&apos;m a passionate fullstack developer specializing in JavaScript
+        technologies, in particular TypeScript, React and Next.js. I love
+        writing robust maintainable code focusing on user experience. I love
+        exploring new and innovative technologies to expand my knowledge base
+        and grow as a developer.
+      </p>
+    </section>
+  );
+}
