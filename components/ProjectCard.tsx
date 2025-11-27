24bc2097080b59f0f56f7f3b5ef9cf29f347dc19
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-27 04:52:12 +0100

    styles: make the image scale on card hover

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -23,43 +23,41 @@ export default function ProjectCard({
   stack,
 }: ProjectCardProps) {
   return (
-    <div>
-      <Card>
-        <CardHeader className="space-y-2">
-          <div className="overflow-hidden z-50 rounded-lg border border-gray-100 dark:border-gray-800">
-            <Image
-              src={imageUrl}
-              alt={`${title} image`}
-              width="700"
-              height="650"
-              className="w-full transition-transform duration-500 hover:scale-110"
-            />
-          </div>
-          <CardTitle className="text-xl font-bold">{title}</CardTitle>
-          <CardDescription className="text-base">{description}</CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-wrap gap-2 items-center">
-          {stack.map((stackItem, index) => (
-            <Badge variant="secondary" key={index} className="text-sm">
-              {stackItem}
-            </Badge>
-          ))}
-        </CardContent>
-        <CardFooter className="space-x-4">
-          <Button variant="outline" asChild>
-            <Link href={githubLink} target="_blank">
-              <Github />
-              <span>Source Code</span>
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href={demoLink} target="_blank">
-              <Link2 />
-              <span>Live Demo</span>
-            </Link>
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card className="group">
+      <CardHeader className="space-y-2">
+        <div className="overflow-hidden z-50 rounded-lg border border-gray-100 dark:border-gray-800">
+          <Image
+            src={imageUrl}
+            alt={`${title} image`}
+            width="700"
+            height="650"
+            className="w-full transition-transform duration-500 group-hover:scale-110"
+          />
+        </div>
+        <CardTitle className="text-xl font-bold">{title}</CardTitle>
+        <CardDescription className="text-base">{description}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-wrap gap-2 items-center">
+        {stack.map((stackItem, index) => (
+          <Badge variant="secondary" key={index} className="text-sm">
+            {stackItem}
+          </Badge>
+        ))}
+      </CardContent>
+      <CardFooter className="space-x-4">
+        <Button variant="outline" asChild>
+          <Link href={githubLink} target="_blank">
+            <Github />
+            <span>Source Code</span>
+          </Link>
+        </Button>
+        <Button asChild>
+          <Link href={demoLink} target="_blank">
+            <Link2 />
+            <span>Live Demo</span>
+          </Link>
+        </Button>
+      </CardFooter>
+    </Card>
   );
 }
