40f56a2e904d39667112f1aced91d048e94ddf20
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-12-25 06:01:19 +0100

    feat: change about me text

--- components/About.tsx
+++ components/About.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 export default function About() {
   return (
     <motion.section
-      className="container py-12 px-4 mx-auto space-y-12 max-w-2xl text-center sm:py-16 lg:py-20"
+      className="container py-12 px-4 mx-auto space-y-12 max-w-3xl text-center sm:py-16 lg:py-20"
       initial={{ opacity: 0, y: 50 }}
       whileInView={{
         opacity: 1,
@@ -13,13 +13,20 @@ export default function About() {
       }}
     >
       <h2 className="text-3xl font-bold">About Me</h2>
-      <p className="text-lg text-gray-500 dark:text-gray-400">
-        I&apos;m a passionate fullstack developer specializing in JavaScript
-        technologies, in particular TypeScript, React and Next.js. I love
-        writing robust maintainable code focusing on user experience. I love
-        exploring new and innovative technologies to expand my knowledge base
-        and grow as a developer.
-      </p>
+      <div className="space-y-4">
+        <p className="text-lg text-gray-500 dark:text-gray-400">
+          I’m a full-stack developer based in Algeria, specializing in
+          JavaScript and TypeScript. I build modern, responsive web applications
+          using React, Next.js, and Node.js, with a strong focus on performance,
+          maintainability, and user experience.
+        </p>
+        <p className="text-lg text-gray-500 dark:text-gray-400">
+          I enjoy working on full-stack projects end-to-end. From designing
+          clean user interfaces to building reliable backends and APIs. I’m
+          always learning new technologies and best practices to improve my
+          craft and write better, more scalable software.
+        </p>
+      </div>
     </motion.section>
   );
 }
