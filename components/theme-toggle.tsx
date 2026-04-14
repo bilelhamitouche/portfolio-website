6ff2d9bcedfaef344952f9540d3966be55ceeb50
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-04-14 03:53:59 +0100

    feat: replace with a button component

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -2,12 +2,13 @@
 
 import { useTheme } from "@/context/theme-context";
 import { LuMoon, LuSun } from "react-icons/lu";
+import Button from "./button";
 
 export default function ThemeToggle() {
   const { theme, toggle } = useTheme();
   return (
-    <button onClick={toggle}>
-      {theme === "dark" ? <LuMoon /> : <LuSun />}
-    </button>
+    <Button variant="ghost" size="icon" onClick={toggle}>
+      {theme === "dark" ? <LuMoon size="20" /> : <LuSun size="20" />}
+    </Button>
   );
 }
